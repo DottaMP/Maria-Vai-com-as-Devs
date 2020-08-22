363c9c54041c62b200bb29496f93661d94258071
GeniferGonzalezSantos <51093689+GeniferGonzalezSantos@users.noreply.github.com>
2020-08-22 19:38:02 -0300

    Revert "adicionou a logica e ajustou ids"

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
 
-
   function numberOfInstallments() {
-
     const installmentsValue = document.querySelector('#installments').value;
     const totalBidAmount = document.querySelector('#bid-amount').value;
-
     const calcNumberOfInstallments = totalBidAmount / installmentsValue;
-    const numberOfInstallments = Math.ceil(calcNumberOfInstallments);
+    const numberOfInstallments = Math.ceil(calcNumberOfInstallments)
+    dueDate(numberOfInstallments)
     document.querySelector('#numberInstallments').innerHTML = numberOfInstallments;
-
     document.getElementById('totalDebtAmount').innerHTML = totalBidAmount;
     document.getElementById('valueInstallments').innerHTML = installmentsValue;
 
-    const duedata = document.getElementById('firstDateInstallment').value;
-    parseDate(duedata)
-
   }
 
-  function parseDate(date) {
+  function dueDate(parcelas) {
+    const valueDate = document.querySelector('#firstDateInstallment').value;
+    parseDate(valueDate, parcelas);
 
-    const dataDigitadaSplit = date.split("-");
-  
-    const day = dataDigitadaSplit[2];
-    const mouth = dataDigitadaSplit[1];
-    const year = dataDigitadaSplit[0];
-    const data =  day + '/' + mouth + '/' + year;
 
-    document.getElementById('dueDate').innerHTML = data;
+    console.log("data é :" + valueDate);
+    console.log();
+  }
+
+  function parseDate(data, parcelas) {
+    let dataDigitadaSplit = data.split("-");
+
+    let dia = dataDigitadaSplit[2];
+    let mes = dataDigitadaSplit[1];
+    let ano = dataDigitadaSplit[0];
+    ano = parseInt(ano);
+    mes = parseInt(mes)
+
+    if (parcelas >= 12) {
+
+      const anos = Math.floor(parcelas / 12);
+      ano = ano + anos;
+      ano;
+      let meses = parcelas % 12;
+      mes = mes + meses;
+
+      if (mes >= 12) {
+        ano++;
+        mes = mes - 12;
+      }
+
+    } if (parcelas < 12) {
+      mes = mes + parcelas;
+    }
     
+    const dataFimpago = new Date(ano, mes, dia);
+    document.getElementById('dueDate').innerHTML = dataFimpago;
   }
 
- 
+
